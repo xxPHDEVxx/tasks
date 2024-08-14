57b2233b38993ef0e96a888c4a17ea526ac84a7f
Rayan <rayanbenaissa@gmail.com>
2024-08-14 02:21:54 +0200

    New_task : add view + fix

---
+++ README.md
@@ -0,0 +1,43 @@
+# Tasks App
+
+## Description
+
+Tasks App is a daily task management application developed entirely in Python. It helps users organize their daily routines with various features for planning, tracking, and analyzing tasks. With an intuitive user interface and sleek design, Tasks App makes it easier to stay productive and achieve your goals.
+
+## Features
+
+- Daily To-Do List: 
+- Easily create and manage your daily tasks.
+- Alerts and Notifications: Receive reminders for important tasks.
+- Task Categorization: Organize your tasks by categories and priority levels.
+- Task Management: Add, delete, modify, organize, and check off completed tasks.
+- Task Analysis: View statistics and analyze your completed tasks over time.
+- Search and Filter System: Quickly search for specific tasks and apply filters to refine your results.
+
+## Screenshots
+
+## Installation
+
+#### 1. Clone the repository
+```bash
+    git clone https://github.com/xxPHDEVxx/tasks.git
+    cd tasks-app
+   ```
+
+#### 2. Install dependencies:
+
+```bash
+    pip install flet
+   ```
+
+#### 3. Run the application:
+
+```bash
+    python To-Do-App.py
+   ```
+    
+## Development in Progress
+
+Tasks App is currently under development. New features and improvements are planned for future releases. Your suggestions and feedback are welcome!
+
+
--- To-Do-App.py
+++ To-Do-App.py
@@ -1,13 +1,14 @@
 from flet import *
 from custom_checkbox import CustomCheckBox
 
+
 # Main function to set up the UI components
 def main(page: Page):
     # Color definitions
-    BG = '#041955'   # Background color
-    FWG = '#97b4ff'  # Foreground color light
-    FG = '#3450a1'   # Foreground color dark
-    PINK = '@eb06ff' # Accent color (Pink)
+    BG = '#18455A'  # Background color
+    FWG = '#F2F2F1'  # Foreground color light
+    FG = '#416272'  # Foreground color dark
+    PINK = '#938783'  # Accent color (Pink)
 
     # Stack to create a circular profile avatar with gradient and image
     circle = Stack(
@@ -16,7 +17,10 @@ def main(page: Page):
                 width=100,
                 height=100,
                 border_radius=50,
-                bgcolor='white12'
+                bgcolor=FG,
+                image_src='Data/Joe.jpeg',
+                image_fit=ImageFit.COVER,
+                expand=True
             )
         ]
     )
@@ -31,12 +35,11 @@ def main(page: Page):
         width=150,
         height=50,
         view_elevation=5,
-        bar_bgcolor=FWG,
-        bar_overlay_color='white',
+        bar_bgcolor=FG,
+        bar_overlay_color='FWG',
         bar_hint_text="task ...",
         visible=False,  # Initially hidden
         scale=transform.Scale(0.8)
-
     )
 
     # Function to shrink the secondary page size when an event occurs
@@ -52,6 +55,14 @@ def main(page: Page):
         page_2.controls[0].scale = transform.Scale(1, alignment=alignment.center_right)
         page.update()
 
+    def add_border(e):
+        # Change the border to black on hover
+        if e.control.bgcolor == BG:
+            e.control.bgcolor = FG
+        else:
+            e.control.bgcolor = BG
+        page.update()  # Update the page to reflect the change
+
     # View for creating a new task, represented by a small "x" button
     create_task_view = Container(
         content=Container(on_click=lambda _: page.go('/'),
@@ -62,7 +73,7 @@ def main(page: Page):
 
     # List of tasks, scrollable and initially empty
     tasks = Column(
-        height=400,
+        height=350,
         scroll='auto',
     )
 
@@ -70,7 +81,7 @@ def main(page: Page):
     for i in range(10):
         tasks.controls.append(
             Container(
-                height=70,
+                height=65,
                 width=400,
                 bgcolor=BG,
                 border_radius=25,
@@ -88,7 +99,7 @@ def main(page: Page):
     )
 
     # Defining categories and adding them to the categories_card row
-    categories = ['Business', 'Family', 'Friends']
+    categories = ['Business', 'Family', 'Friends', 'Bike', 'Sport', 'Meditation', 'Coding', 'Learning', 'Shopping']
     for i, category in enumerate(categories):
         categories_card.controls.append(
             Container(
@@ -100,11 +111,11 @@ def main(page: Page):
                 content=Column(
                     controls=[
                         Text('40 Tasks'),  # Placeholder task count
-                        Text(category),   # Category name
+                        Text(category),  # Category name
                         Container(
                             width=160,
                             height=5,
-                            bgcolor='white12',
+                            bgcolor=FWG,
                             border_radius=20,
                             padding=padding.only(right=i * 30),
                             content=Container(
@@ -123,14 +134,15 @@ def main(page: Page):
                 Row(alignment='spaceBetween',
                     controls=[
                         Container(on_click=lambda e: shrink(e),
-                                  content=Icon(icons.MENU)  # Shrink the secondary page
+                                  content=Icon(icons.MENU, color=FWG)  # Shrink the secondary page
                                   ),
                         Row(
                             controls=[
                                 Container(on_click=lambda e: show_search_bar(e),
-                                          content=Icon(icons.SEARCH)  # Shrink the secondary page
+                                          content=Icon(icons.SEARCH, color=FWG)  # Shrink the secondary page
                                           ),  # Search icon
-                                search_bar,                                Icon(icons.NOTIFICATIONS_OUTLINED)  # Notifications icon
+                                search_bar,
+                                Icon(icons.NOTIFICATIONS_OUTLINED, color=FWG)  # Notifications icon
                             ],
                         ),
                     ],
@@ -147,10 +159,15 @@ def main(page: Page):
                 Stack(
                     controls=[
                         tasks,  # List of tasks
-                        FloatingActionButton(
-                            icon=icons.ADD, on_click=lambda _: page.go('/create_task')  # Add task button
-                        )
                     ]
+                ),
+                Container(
+                    padding=padding.only(top=10),
+                    alignment=alignment.center,
+                    content=FloatingActionButton(
+                        bgcolor=BG,
+                        icon=icons.ADD, on_click=lambda _: page.go('/create_task')  # Add task button
+                    )
                 )
             ]
         )
@@ -168,7 +185,7 @@ def main(page: Page):
                 Row(
                     controls=[
                         Container(on_click=lambda e: undo_shrink(e),
-                                  content=Icon(icons.ARROW_BACK_IOS)  # Back button to undo shrink
+                                  content=Icon(icons.ARROW_BACK_IOS, color=FWG)  # Back button to undo shrink
                                   )
                     ]
                 ),
@@ -177,18 +194,40 @@ def main(page: Page):
                 Text('Joe\nJackson', size=32, weight='bold'),  # User name
                 Container(height=25),
                 Row(controls=[
-                    Icon(icons.FAVORITE_BORDER_SHARP, color='white12'),  # Favorite icon
-                    Text('Templates', size=15, width=FontWeight.W_300, color='white', font_family='poppins')
+                    Container(
+                        on_click=lambda _: page.go('/'),
+                        content=Row(
+                            controls=[
+                                Icon(icons.SETTINGS, color=FWG),  # Favorite icon
+                                Text('Settings', size=15, width=FontWeight.W_300, color='white', font_family='poppins')
+                            ]
+                        )
+                    )
                 ]),
                 Container(height=5),
                 Row(controls=[
-                    Icon(icons.CARD_TRAVEL, color='white12'),  # Categories icon
-                    Text('Categories', size=15, width=FontWeight.W_300, color='white', font_family='poppins')
+                    Container(
+                        on_click=lambda _: page.go('/'),
+                        content=Row(
+                            controls=[
+                                Icon(icons.CARD_TRAVEL, color=FWG),  # Categories icon
+                                Text('Categories', size=15, width=FontWeight.W_300, color='white',
+                                     font_family='poppins')
+                            ]
+                        )
+                    )
                 ]),
                 Container(height=5),
                 Row(controls=[
-                    Icon(icons.CALCULATE_OUTLINED, color='white12'),  # Analytics icon
-                    Text('Analytics', size=15, width=FontWeight.W_300, color='white', font_family='poppins')
+                    Container(
+                        on_click=lambda _: page.go('/'),
+                        content=Row(
+                            controls=[
+                                Icon(icons.CALCULATE_OUTLINED, color=FWG),  # Analytics icon
+                                Text('Analytics', size=15, width=FontWeight.W_300, color='white', font_family='poppins')
+                            ]
+                        )
+                    )
                 ])
             ]
         )
@@ -212,7 +251,7 @@ def main(page: Page):
                          )
                      )
                  ]
-    )
+                 )
 
     # Main container that holds both pages
     container = Container(
@@ -223,7 +262,147 @@ def main(page: Page):
         content=Stack(
             controls=[
                 page_1,  # Profile and menu page
-                page_2   # Task management page
+                page_2  # Task management page
+            ]
+        )
+    )
+
+    categories_container = Column()
+
+    # Loop to create rows of three categories
+    row_controls = []
+    for i, category in enumerate(categories):
+        # Add each category to a temporary list
+        row_controls.append(
+            Container(
+                border_radius=25,
+                bgcolor=FG,
+                content=FloatingActionButton(
+                    width=105,
+                    height=50,
+                    bgcolor=FG,
+                    content=Text(category, size=14, color=FWG, text_align='center'),
+                    on_click=lambda e: add_border(e),  # Apply border on hover
+                ),
+            )
+        )
+
+        # When three categories are added or it's the last category
+        if len(row_controls) == 3 or i == len(categories) - 1:
+            # Add the Row containing the three (or fewer) categories
+            categories_container.controls.append(
+                Row(
+                    controls=row_controls,
+                    alignment='spaceBetween'  # Space between the buttons to balance
+                )
+            )
+            # Reset the temporary list for the next Row
+            row_controls = []
+
+    # New task view
+    create_task_view = Container(
+        width=400,
+        height=850,
+        bgcolor=FG,
+        border_radius=35,
+        padding=padding.only(top=50),
+        content=Column(
+            controls=[
+                # First main container
+                Container(
+                    expand=True,  # Make this container fill the remaining space
+                    padding=padding.all(25),
+                    content=Column(
+                        controls=[
+                            Row(alignment='spaceBetween',
+                                controls=[
+                                    Container(on_click=lambda e: page.go('/'),
+                                              content=Icon(icons.ARROW_BACK_IOS, color=FWG)  # Go back
+                                              ),
+                                    Container(
+                                        Text('Create New Task', size=25, weight='bold')
+                                    ),
+                                    Container()  # Placeholder for spacing
+                                ],
+                                ),
+                            Container(height=50),  # Space between rows
+                            Row(
+                                controls=[
+                                    Text('Title', size=15),
+                                    TextField(border_radius=10, border_color=FWG),
+                                ]
+                            ),
+                            Container(height=10),  # Additional spacing
+                        ]
+                    )
+                ),
+                # Second container with the last row
+                Container(
+                    bgcolor=FWG,
+                    border_radius=35,
+                    content=Row(
+                        controls=[
+                            Container(
+                                height=570,
+                                width=400,
+                                bgcolor=FWG,
+                                border_radius=35,
+                                content=Container(
+                                    content=Column(
+                                        controls=[
+                                            Row(
+                                                controls=[
+                                                    Container(
+                                                        padding=padding.only(left=25, top=40, right=25),
+                                                        content=Text('Categories', size=15, color='black',
+                                                                     weight='bold')
+                                                    )
+                                                ]
+                                            ),
+
+                                            Row(
+                                                controls=[
+                                                    Column(
+                                                        controls=[
+                                                            Container(
+                                                                padding=padding.only(left=40, right=40, bottom=50,
+                                                                                     top=20),
+                                                                content=categories_container
+                                                            )
+                                                        ]
+                                                    )
+                                                ]
+                                            ),
+                                            Row(
+                                                controls=[
+                                                    Column(
+                                                        controls=[
+                                                            Container(
+                                                                bgcolor=FG,
+                                                                width=300,
+                                                                margin=margin.all(50),
+                                                                alignment=alignment.center,
+                                                                height=70,
+                                                                border_radius=35,
+                                                                content=FloatingActionButton(
+                                                                    bgcolor=FG,
+                                                                    width=300,
+                                                                    height=70,
+                                                                    content=Text('Create Task', size=25),
+                                                                    on_click=lambda _: page.go('/')
+                                                                )
+                                                            )
+                                                        ]
+                                                    )
+                                                ]
+                                            )
+                                        ]
+                                    )
+                                )
+                            )
+                        ]
+                    )
+                )
             ]
         )
     )
@@ -254,6 +433,7 @@ def main(page: Page):
     page.add(container)  # Add the main container to the page
     page.on_route_change = route_change  # Set the route change handler
     page.go(page.route)  # Navigate to the current route
+
 
 # Run the app
 app(target=main)
